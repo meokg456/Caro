d84907f2bd0cb7c52267f682be9a6b558696a37a
meokg456 <43285844+meokg456@users.noreply.github.com>
2018-09-30 16:45:45 +0700

    Hữu Dũng - Thêm class common

--- Caro/_Common.h
+++ Caro/_Common.h
@@ -1,8 +1,18 @@
 #pragma once
-class _Common
-{
+class _Common {
 public:
-	_Common();
-	~_Common();
+	static void fixConsoleWindow();
+	static void gotoXY(int, int);
 };
+void _Common::gotoXY(int pX, int pY) {
+	COORD coord;
+	coord.X = pX;
+	coord.Y = pY;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+void _Common::fixConsoleWindow() {
+	HWND consoleWindow = GetConsoleWindow();
+	LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
+	style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
+	SetWindowLong(consoleWindow, GWL_STYLE, style);}
 
