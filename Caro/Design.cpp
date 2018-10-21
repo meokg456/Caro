dc64b719b0eb286c3294835d5b14a4f234fa2037
Dung <meokg456@gmail.com>
2018-10-21 10:57:05 +0700

    Menu

--- Caro/Design.cpp
+++ Caro/Design.cpp
@@ -51,50 +51,168 @@ void Design::setFontSize(int FontSize)
 	}
 }
 
-void Design::CaroWord()
-{
-	int x, y;
-	x = 55;
-	y = 10;
-
-	
-	_Common::gotoXY(x, y);
+void Design::CaroWord(int x,int y)
+{	
 	Design::SetColor(12);
+	_Common::gotoXY(x, y);
 	cout << " +      +               +         +                ++    ++    ++";
 	_Common::gotoXY(x, y+1);
 	cout << " _______    +       +          _______ _   \\\\/ \\/ \\//           +      ";
 	_Common::gotoXY(x, y+2);
 	cout << "(_______)                     (_______) |   \\______/               +  +";
-		_Common::gotoXY(x, y+3);
-		cout << "_         _____  ____ ___      _      | |__  _____  ___  ___     +      ";
-		_Common::gotoXY(x, y+4);
-		cout << "|  |  +  (____ |/ ___)  _ \\   | |  +  |  _ \\| ___ |/___)/___)         +  ";
-		_Common::gotoXY(x, y+5);
-		cout << "|  |_____/ ___ | |   | |_| |  | |_____| | | | ____|___ |___ |  (\\/)";
-		_Common::gotoXY(x, y+6);
-		cout << " \\______)_____ |_|   \\_____/  \\_______)_| |_|_____|___/(___/   (/\\) O";
-		_Common::gotoXY(x, y+7);
-		cout << "  +       +                                                           +   ";
-		_Common::gotoXY(x, y+8);
-		cout << "     +          +             +               +               +    +     ";
-
-		/*cout << " +      +               +         +                ++    ++    ++" << endl;
-		cout << " _______    +       +          _______ _   \\/ \/ \//           +      " << endl;
-		cout << "(_______)                     (_______) |   \______/               +  +" << endl;
-		cout << "_         _____  ____ ___      _      | |__  _____  ___  ___     +      " << endl;
-		cout << "|  |  +  (____ |/ ___)  _ \   | |  +  |  _ \| ___ |/___)/___)         +  " << endl;
-		cout << "|  |_____/ ___ | |   | |_| |  | |_____| | | | ____|___ |___ |  (\/)" << endl;
-		cout << " \______)_____ |_|    \___/    \______)_| |_|_____|___/(___/   (/\) O" << endl;
-		cout << "  +       +                                                           +   " << endl;
-		cout << "     +          +             +               +               +    +     " << endl;*/
+	_Common::gotoXY(x, y + 3);
+	cout << "_         _____  ____ ___      _      | |__  _____  ___  ___     +      ";
+	_Common::gotoXY(x, y + 4);
+	cout << "|  |  +  (____ |/ ___)  _ \\   | |  +  |  _ \\| ___ |/___)/___)         +  ";
+	_Common::gotoXY(x, y + 5);
+	cout << "|  |_____/ ___ | |   | |_| |  | |_____| | | | ____|___ |___ |  (\\/)";
+	_Common::gotoXY(x, y + 6);
+	cout << " \\______)_____ |_|   \\_____/  \\_______)_| |_|_____|___/(___/   (/\\) O";
+	_Common::gotoXY(x, y + 7);
+	cout << "  +       +                                                            +";
+	_Common::gotoXY(x, y+8);
+	cout << "     +          +             +               +               +    +     ";
+	Design::SetColor(15);//Trả lại màu trắng cho màn hình console
 }
-void Design::Menu()
+void Design::Menu(int left,int top,int size)
 {
-	int x = 78;
-	int y = 35;
-	Design::SetColor(13);
-	_Common::gotoXY(x, y);
-	cout << char(175) << "     New game     " << char(174);
+	Design::SetColor(11);
+	_Common::gotoXY(left, top);
+
+	int outofleft;
+	int outofright;
+
+	//Vẽ khung menu
+	for (int i = 1; i <= size; i++) {
+		//Vẽ viền trái 
+		_Common::gotoXY(left, top+i);
+		cout << char(179);
+		_Common::gotoXY(left, top + i+1);
+		cout << char(179);
+
+		//Vẽ viền trái ngoài
+		outofleft = left - 3;
+		outofright = top - 2;
+		_Common::gotoXY(outofleft, outofright + i);
+		cout << char(179);
+		_Common::gotoXY(outofleft, outofright + i + 1);
+		cout << char(179);
+		_Common::gotoXY(outofleft, outofright + i + 2);
+		cout << char(179);
+		_Common::gotoXY(outofleft, outofright + i + 3);
+		cout << char(179);
+		_Common::gotoXY(outofleft, outofright + i + 4);
+		cout << char(179);
+		_Common::gotoXY(outofleft, outofright + i + 5);
+		cout << char(179);
+		//Vẽ viền phải
+		_Common::gotoXY(left+ size*2, top+i);
+		cout << char(179);
+		_Common::gotoXY(left+ size*2, top +i+1);
+		cout << char(179);
+
+		//Vẽ viền phải ngoài
+		outofleft = left +size*2+3;
+		outofright = top - 2;
+		_Common::gotoXY(outofleft, outofright + i);
+		cout << char(179);
+		_Common::gotoXY(outofleft, outofright + i + 1);
+		cout << char(179);
+		_Common::gotoXY(outofleft, outofright + i + 2);
+		cout << char(179);
+		_Common::gotoXY(outofleft, outofright + i + 3);
+		cout << char(179);
+		_Common::gotoXY(outofleft, outofright + i + 4);
+		cout << char(179);
+		_Common::gotoXY(outofleft, outofright + i + 5);
+		cout << char(179);
+
+		//Vẽ viền trên
+		_Common::gotoXY(left+i, top);
+		cout << "_____________________________";
+
+		//Vẽ viền trên ngoài
+		outofleft = left - 3;
+		_Common::gotoXY(outofleft + i, outofright);
+		cout << "___________________________________";
+
+		//Vẽ viền dưới
+		_Common::gotoXY(left + i, top +size+1);
+		cout << "_____________________________";
+
+		//Vẽ viền dưới ngoài
+		outofleft = left - 3;
+		outofright = top + size + 1;
+		_Common::gotoXY(outofleft + i, outofright+2);
+		cout << "___________________________________";
+	}
+
 	//_Common::gotoXY(x, y);
 	//cout << char(32) << "     New game     " << char(32) << endl;
+
+	Design::SetColor(14);
+	int inofleft= left + 25;
+	int inofright = top + 7;
+	_Common::gotoXY(inofleft, inofright);
+	cout << " New Game";
+	_Common::gotoXY(inofleft, inofright += 5);
+	cout << " One Player";
+	_Common::gotoXY(inofleft, inofright += 5);
+	cout << " Exit Game";
+}
+
+int Design::RunMenu(int left, int top, int size)
+{
+	Menu(left, top, size);
+	int lleft = left + 21;
+	int ltop = top + 2;
+	int i = 1;
+	int NUMLIST = 3;
+	ConTro(lleft, ltop, i);
+	while (true) {
+
+		int key = _getch();
+		XoaTro(lleft, ltop, i);
+
+		switch (key)
+		{
+		case 13:
+		{
+			return i;
+			break;
+		}
+		case'S':case's':
+		{
+			i++;
+
+			break;
+		}
+		case'W':case'w':
+		{
+	
+			i--;
+
+			break;
+		}
+
+		}
+		if (i == 0) i = NUMLIST;
+		if (i > NUMLIST) i = 1;
+		ConTro(lleft, ltop, i);
+	}
+}
+void Design::XoaTro(int left, int top, int k)
+{
+	_Common::gotoXY(left, top + k * 5);
+	cout << char(32);
+	_Common::gotoXY(left + 17, top + k * 5);
+	cout << char(32);
+}
+void Design::ConTro(int left, int top, int k)
+{
+	_Common::gotoXY(left, top + 5*k);
+	cout << char(175);
+	_Common::gotoXY(left +17, top + 5*k);
+	cout << char(174);
+
 }
--- Caro/Design.h
+++ Caro/Design.h
@@ -26,8 +26,11 @@ public:
 	static void SetColor(unsigned short);
 	static void LoadingWord();//Hiển thị loading game
 	static void setFontSize(int FontSize);
-	static void CaroWord();
-	static void Menu();
+	static void CaroWord(int,int);
+	static void Menu(int ,int,int);
+	static int RunMenu(int left, int top, int size);
+	static void XoaTro(int left, int top, int k);
+	static void ConTro(int left, int top, int k);
 };
 
 
